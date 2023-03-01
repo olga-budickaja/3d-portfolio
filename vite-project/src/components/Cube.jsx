39dda562c32e67967ece5f13ebf7f7955174652e
Olga Buditska <olgabudickaja@gmail.com>
2023-03-01 22:23:40 +0200

    add 3d cube on who section

--- vite-project/src/components/Cube.jsx
+++ vite-project/src/components/Cube.jsx
@@ -7,16 +7,16 @@ const Cube = () => {
     useFrame(state => (textRef.current.position.x = Math.sin(state.clock.elapsedTime) * 2))
     return (
         <mesh>
-            <boxGeometry args={[2, 2, 2]}/>
+            <boxGeometry />
             <meshStandardMaterial>
                 <RenderTexture attach="map">
                     <PerspectiveCamera
                         makeDefault
-                        position={[0,0,2]}
+                        position={[0,0,5]}
                     />
                     <color attach="background" args={["#ff99ff"]} />
-                    <Text ref={textRef} fontSize={0.9} color="#555">
-                        hello
+                    <Text ref={textRef} fontSize={1.9} color="#555">
+                        :) hello :)
                     </Text>
                 </RenderTexture>
             </meshStandardMaterial>
--- vite-project/src/components/who/Who.jsx
+++ vite-project/src/components/who/Who.jsx
@@ -1,13 +1,21 @@
 import { Container, Description, Line, Subtitle, Title, WhatWeDo } from "../hero/HeroStyle.js";
 import { Section, Left, Right } from "./WhoStyle";
 import { Button } from "../navbar/NavbarStyle.js";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import Cube from "../Cube.jsx";
 
 const Who = () => {
     return (
         <Section>
             <Container>
                 <Left>
-                    {/*3d model*/}
+                    <Canvas camera={{ fov: 25, position: [5, 5, 5] }}>
+                        <OrbitControls enableZoom={false} autoRotate={true}/>
+                        <ambientLight intensity={1}/>
+                        <directionalLight position={[3, 2, 1]} />
+                        <Cube />
+                    </Canvas>
                 </Left>
                 <Right>
                     <Title>Step outside the box.</Title>
