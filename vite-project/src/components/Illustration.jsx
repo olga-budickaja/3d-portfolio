fdc6e85297d744080acc83d6f195e4e11bd6a11e
Olga Buditska <olgabudickaja@gmail.com>
2023-03-02 20:17:14 +0200

    added desc

--- vite-project/src/components/Illustration.jsx
+++ vite-project/src/components/Illustration.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Stage } from "@react-three/drei";
 import Laptop from "./Laptop.jsx";
+import { Desc } from "../Desc.js";
 
 const Illustration = () => {
     return (
-        <Canvas>
-            <Stage environment="city" intensity={0.6}>
-                <Laptop />
-            </Stage>
-            <OrbitControls enableZoom={false} autoRotate />
-        </Canvas>
+        <>
+            <Canvas>
+                <Stage environment="city" intensity={0.6}>
+                    <Laptop />
+                </Stage>
+                <OrbitControls enableZoom={false} autoRotate />
+            </Canvas>
+            <Desc>We show illustrations in 3D format from all angles.</Desc>
+        </>
+
     );
 };
 
--- vite-project/src/components/ProductDesign.jsx
+++ vite-project/src/components/ProductDesign.jsx
@@ -1,15 +1,20 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Stage } from "@react-three/drei";
 import Bag from "./Bag";
+import { Desc } from "../Desc.js";
 
 const ProductDesign = () => {
     return (
-        <Canvas>
-            <Stage environment="city" intensity={0.6}>
-                <Bag />
-            </Stage>
-            <OrbitControls enableZoom={false} />
-        </Canvas>
+        <>
+            <Canvas>
+                <Stage environment="city" intensity={0.6}>
+                    <Bag />
+                </Stage>
+                <OrbitControls enableZoom={false} />
+            </Canvas>
+            <Desc>Your product will be viewed from all sides in 3D format :)</Desc>
+        </>
+
     );
 };
 
